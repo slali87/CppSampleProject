a248d091518a48a75dd407085020c690536e6110
slali87 <109802779+slali87@users.noreply.github.com>
2023-03-27 17:39:49 +0200

    TOOLS: Add a specific NO_LINT into test part
    Add a specific (cppcoreguidelines-avoid-non-const-global-variables)
     NO_LINT into test part.

--- test/LibTestCases.cpp
+++ test/LibTestCases.cpp
@@ -2,7 +2,7 @@
 #include "gtest/gtest.h"
 
 // Silence because these warnings based on using google test
-// NOLINTNEXTLINE(cert-err58-cpp, cppcoreguidelines-owning-memory, fuchsia-default-arguments-calls, fuchsia-statically-constructed-objects)
+// NOLINTNEXTLINE(cert-err58-cpp, cppcoreguidelines-owning-memory, fuchsia-default-arguments-calls, fuchsia-statically-constructed-objects, cppcoreguidelines-avoid-non-const-global-variables)
 TEST(LibTestSuite, testHelloWorld_ReturnExpectedString)
 {
     Lib lib;
