a7c84077570c9ef5d11b84a7424c1fdd5f428db3
slali87 <109802779+slali87@users.noreply.github.com>
2024-10-04 22:42:36 +0200

    TOOLS: Fix CI 3
    Fix CI 3

--- cmake/Setup.cmake
+++ cmake/Setup.cmake
@@ -4,8 +4,8 @@ cmake_minimum_required(VERSION 3.28)
 execute_process(COMMAND conan profile detect --force WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
 
 # install deps
-execute_process(COMMAND conan install . -b=missing
-                COMMAND conan install . -s "&:build_type=Debug" WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND conan install . -b=missing WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND conan install . -s "&:build_type=Debug" WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
 
 # set git hooks
 execute_process(COMMAND git config core.hooksPath ${CMAKE_SOURCE_DIR}/tools/git/hooks
