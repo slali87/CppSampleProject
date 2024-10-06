3d0ff5f3cf921c639ccc75bc042c4b5bec507afe
slali87 <109802779+slali87@users.noreply.github.com>
2024-10-06 16:40:47 +0200

    TOOLS: Fix CI 6
    Fix CI 6

--- cmake/Setup.cmake
+++ cmake/Setup.cmake
@@ -1,11 +1,13 @@
 cmake_minimum_required(VERSION 3.28)
 
-#set config settings
-execute_process(COMMAND conan profile detect --force WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
-
 # install deps
-execute_process(COMMAND conan install . -b=missing WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
-execute_process(COMMAND conan install . -s "&:build_type=Debug" WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
+if(UNIX)
+  set(Profile "${CMAKE_SOURCE_DIR}/tools/conan/profileLinux")
+elseif(WIN32)
+  set(Profile "${CMAKE_SOURCE_DIR}/tools/conan/profileWindows")
+endif()
+execute_process(COMMAND conan install . -b=missing -pr:a=${Profile} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND conan install . -s "&:build_type=Debug" -pr:a=${Profile} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY)
 
 # set git hooks
 execute_process(COMMAND git config core.hooksPath ${CMAKE_SOURCE_DIR}/tools/git/hooks COMMAND_ERROR_IS_FATAL ANY)
